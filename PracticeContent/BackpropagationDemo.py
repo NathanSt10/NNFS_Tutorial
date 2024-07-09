da3168fd1f5c052d542c33a1e74b61f9229dd11f
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2024-07-09 16:24:38 -0500

    Backpropagation Demo with one layer

--- PracticeContent/BackpropagationDemo.py
+++ PracticeContent/BackpropagationDemo.py
@@ -1,40 +1,18 @@
-x = [1.0, -2.0, 3.0]
-w = [-3.0, -1.0, 2.0]
-b = 1.0
+import numpy as np
 
-xw0 = x[0] * w[0]
-xw1 = x[1] * w[1]
-xw2 = x[2] * w[2]
+dvalues = np.array([[1., 1., 1.]])
 
-z = xw0 + xw1 + xw2 + b
+weights = np.array([[0.2, 0.8, -0.5, 1],
+                    [0.5, -0.91, 0.26, -0.5],
+                    [-0.26, -0.27, 0.17, 0.87]]).T
 
-y = max(z, 0)
-
-dvalue = 1.0
-
-drelu_dz = dvalue * (1 if z > 0 else 0)
-print(drelu_dz)
-
-dsum_dxw0 = 1
-dsum_dxw1 = 1
-dsum_dxw2 = 1
-dsum_db = 1
-drelu_dxw0 = drelu_dz * dsum_dxw0
-drelu_dxw1 = drelu_dz * dsum_dxw1
-drelu_dxw2 = drelu_dz * dsum_dxw2
-drelu_db = drelu_dz * dsum_db
-print(drelu_dxw0, drelu_dxw1, drelu_dxw2, drelu_db)
-
-dmul_dx0 = w[0]
-dmul_dx1 = w[1]
-dmul_dx2 = w[2]
-dmul_dw0 = x[0]
-dmul_dw1 = x[1]
-dmul_dw2 = x[2]
-drelu_dx0 = drelu_dxw0 * dmul_dx0
-drelu_dw0 = drelu_dxw0 * dmul_dw0
-drelu_dx1 = drelu_dxw1 * dmul_dx1
-drelu_dw1 = drelu_dxw1 * dmul_dw1
-drelu_dx2 = drelu_dxw2 * dmul_dx2
-drelu_dw2 = drelu_dxw2 * dmul_dw2
-print(drelu_dx0, drelu_dw0, drelu_dx1, drelu_dw1, drelu_dx2, drelu_dw2)
+dx0 = sum([weights[0][0] * dvalues[0][0], weights[0][1] * dvalues[0][1],
+           weights[0][2] * dvalues[0][2]])
+dx1 = sum([weights[1][0] * dvalues[0][0], weights[1][1] * dvalues[0][1],
+           weights[1][2] * dvalues[0][2]])
+dx2 = sum([weights[2][0] * dvalues[0][0], weights[2][1] * dvalues[0][1],
+           weights[2][2] * dvalues[0][2]])
+dx3 = sum([weights[3][0] * dvalues[0][0], weights[3][1] * dvalues[0][1],
+           weights[3][2] * dvalues[0][2]])
+dinputs = np.array([dx0, dx1, dx2, dx3])
+print(dinputs)
