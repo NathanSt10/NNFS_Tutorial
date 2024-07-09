44d11baa2405be68862d4c187d338b30f739498d
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2024-07-09 16:37:34 -0500

    Calculated individual partial derivatives

--- PracticeContent/BackpropagationDemo.py
+++ PracticeContent/BackpropagationDemo.py
@@ -1,18 +1,35 @@
 import numpy as np
 
-dvalues = np.array([[1., 1., 1.]])
+dvalues = np.array([[1., 1., 1.],
+                    [2., 2., 2.],
+                    [3., 3., 3.]])
 
 weights = np.array([[0.2, 0.8, -0.5, 1],
                     [0.5, -0.91, 0.26, -0.5],
                     [-0.26, -0.27, 0.17, 0.87]]).T
 
-dx0 = sum([weights[0][0] * dvalues[0][0], weights[0][1] * dvalues[0][1],
-           weights[0][2] * dvalues[0][2]])
-dx1 = sum([weights[1][0] * dvalues[0][0], weights[1][1] * dvalues[0][1],
-           weights[1][2] * dvalues[0][2]])
-dx2 = sum([weights[2][0] * dvalues[0][0], weights[2][1] * dvalues[0][1],
-           weights[2][2] * dvalues[0][2]])
-dx3 = sum([weights[3][0] * dvalues[0][0], weights[3][1] * dvalues[0][1],
-           weights[3][2] * dvalues[0][2]])
-dinputs = np.array([dx0, dx1, dx2, dx3])
+dinputs = np.dot(dvalues, weights.T)
 print(dinputs)
+
+inputs = np.array([[1, 2, 3, 2.5],
+                   [2., 5., -1., 2],
+                   [-1.5, 2.7, 3.3, -0.8]])
+dweights = np.dot(inputs.T, dvalues)
+print(dweights)
+
+biases = np.array([[2, 3, 0.5]])
+dbiases = np.sum(dvalues, axis=0, keepdims=True)
+print(dbiases)
+
+# Example layer output
+z = np.array([[1, 2, -3, -4],
+              [2, -7, -1, 3],
+              [-1, 2, 5, -1]])
+
+dvalues = np.array([[1, 2, 3, 4],
+                    [5, 6, 7, 8],
+                    [9, 10, 11, 12]])
+
+drelu = dvalues.copy()
+drelu[z <= 0] = 0
+print(drelu)
