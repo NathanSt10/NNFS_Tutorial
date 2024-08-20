742e3f2fbd14a57fb5f2868f16ae2642cbd3f4d5
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2024-08-20 15:52:47 -0500

    Adam Optimizer

--- PracticeNeuralNetworks/ModelWithOptimizer.py
+++ PracticeNeuralNetworks/ModelWithOptimizer.py
@@ -11,8 +11,8 @@ class Layer_Dense:
         self.biases = np.zeros((1, n_neurons))
 
     def forward(self, inputs):
-        self.output = np.dot(inputs, self.weights) + self.biases
         self.inputs = inputs
+        self.output = np.dot(inputs, self.weights) + self.biases
 
     def backward(self, dvalues):
         self.dweights = np.dot(self.inputs.T, dvalues)
@@ -100,36 +100,40 @@ class Activation_Softmax_Loss_CategoricalCrossEntropy:
         self.dinputs = self.dinputs / samples
 
 
-class Optimizer_SGD:
-    def __init__(self, learning_rate=1.0, decay=0., momentum=0.):
+class Optimizer_Adam:
+    def __init__(self, learning_rate=0.001, decay=0., epsilon=1e-7, beta_1=0.9, beta_2=0.999):
         self.learning_rate = learning_rate
         self.current_learning_rate = learning_rate
         self.decay = decay
         self.iterations = 0
-        self.momentum = momentum
+        self.epsilon = epsilon
+        self.beta_1 = beta_1
+        self.beta_2 = beta_2
 
     def pre_update_params(self):
         if self.decay:
             self.current_learning_rate = self.learning_rate * (1. / (1. + self.decay * self.iterations))
 
     def update_params(self, layer):
-        if self.momentum:
-            if not hasattr(layer, 'weight_momentums'):
-                layer.weight_momentums = np.zeros_like(layer.weights)
-                layer.bias_momentums = np.zeros_like(layer.biases)
+        if not hasattr(layer, 'weight_cache'):
+            layer.weight_momentums = np.zeros_like(layer.weights)
+            layer.weight_cache = np.zeros_like(layer.weights)
+            layer.bias_momentums = np.zeros_like(layer.biases)
+            layer.bias_cache = np.zeros_like(layer.biases)
 
-            weight_updates = self.momentum * layer.weight_momentums - self.current_learning_rate * layer.dweights
-            layer.weight_momentums = weight_updates
+        layer.weight_momentums = self.beta_1 * layer.weight_momentums + (1 - self.beta_1) * layer.dweights
+        layer.bias_momentums = self.beta_1 * layer.bias_momentums + (1 - self.beta_1) * layer.dbiases
+        weight_momentums_corrected = layer.weight_momentums / (1 - self.beta_1 ** (self.iterations + 1))
+        bias_momentums_corrected = layer.bias_momentums / (1 - self.beta_1 ** (self.iterations + 1))
+        layer.weight_cache = self.beta_2 * layer.weight_cache + (1 - self.beta_2) * layer.dweights**2
+        layer.bias_cache = self.beta_2 * layer.bias_cache + (1 - self.beta_2) * layer.dbiases**2
+        weight_cache_corrected = layer.weight_cache / (1 - self.beta_2 ** (self.iterations + 1))
+        bias_cache_corrected = layer.bias_cache / (1 - self.beta_2 ** (self.iterations + 1))
 
-            bias_updates = self.momentum * layer.bias_momentums - self.current_learning_rate * layer.dbiases
-            layer.bias_momentums = bias_updates
-
-        else:
-            weight_updates += -self.current_learning_rate * layer.dweights
-            bias_updates += -self.current_learning_rate * layer.dbiases
-
-        layer.weights += weight_updates
-        layer.biases += bias_updates
+        layer.weights += (-self.current_learning_rate * weight_momentums_corrected /
+                          (np.sqrt(weight_cache_corrected) + self.epsilon))
+        layer.biases += (-self.current_learning_rate * bias_momentums_corrected /
+                         (np.sqrt(bias_cache_corrected) + self.epsilon))
 
     def post_update_params(self):
         self.iterations += 1
@@ -151,7 +155,7 @@ dense2 = Layer_Dense(64, 4)
 loss_activation = Activation_Softmax_Loss_CategoricalCrossEntropy()
 
 # optimizer
-optimizer = Optimizer_SGD(decay=1e-3, momentum=0.9)
+optimizer = Optimizer_Adam(learning_rate=0.05, decay=5e-7)
 
 for epoch in range(10001):
 
